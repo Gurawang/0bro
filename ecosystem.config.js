1cdb8906c72dab76c972c3a0b414974845e1a4e0
Your NameGurawang <your_email@example.comljsljs3515@naver.com>
2025-01-02 04:06:31 +0900

    Fix node_modules tracking issue and add Caddyfile

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     apps: [{
         name: "server",
-        script: "./server.js", // 서버의 진입점 파일 경로
+        script: "C:\\Users\\Administrator\\0bro\\server.js", // 절대 경로로 설정
         env: {
-            PORT: 5000 // Express 서버에서 사용할 포트를 명시
+            PORT: 5000
         },
-        instances: 1, // 여러 개의 인스턴스를 사용하지 않고 하나만 실행
-        autorestart: true, // 서버가 꺼졌을 때 자동 재시작
-        watch: false // 코드 변경 감지 시 자동 재시작 비활성화
+        instances: 1,
+        autorestart: true,
+        watch: false
     }]
 };
